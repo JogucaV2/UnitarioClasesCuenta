0f7811c87a7d04c2bf84042d278ef1e389eca5df
JogucaV2 <joguca664@gmail.com>
2026-04-17 16:03:19 +0200

    Añadido un comentario de prueba en el método setNombre de la clase Producto

---
+++ Test/practica/CuentaBancariaTest.java
@@ -0,0 +1,66 @@
+package practica;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaBancariaTest {
+
+    @Test
+    void getTitular() {
+    }
+
+    @Test
+    void setTitular() {
+    }
+
+    @Test
+    void getSaldo() {
+    }
+
+    @Test
+    void setSaldo() {
+    }
+
+    @Test
+    void isBloqueada() {
+    }
+
+    @Test
+    void setBloqueada() {
+    }
+
+    @Test
+    void getTotalCuentasCreadas() {
+    }
+
+    @Test
+    void reiniciarContador() {
+    }
+
+    @Test
+    void ingresar() {
+    }
+
+    @Test
+    void retirar() {
+    }
+
+    @Test
+    void aplicarComisionMensual() {
+    }
+
+    @Test
+    void movimientosValidos() {
+    }
+
+    @Test
+    void bloquearSiSaldoCero() {
+    }
+
+    @Test void testConstructor() {
+
+    }
+    @Test void testConstructor2() {
+    }
+}
